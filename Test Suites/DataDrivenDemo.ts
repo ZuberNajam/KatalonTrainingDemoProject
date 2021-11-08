1e004a6c5ea9353d3c819afc7dccd249124ed6b5
Zuber <zsyednajam@gmail.com>
2021-11-08 13:13:58 -0500

    My 1st project commit

---
+++ Test Suites/DataDrivenDemo.ts
@@ -0,0 +1,41 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>DataDrivenDemo</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>93c01910-8bea-467c-b401-279fb73a40c4</testSuiteGuid>
+   <testCaseLink>
+      <guid>74823b7c-9487-42d9-aff3-9792563ca1e8</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Demo/DataDrivenTestingDemo</testCaseId>
+      <testDataLink>
+         <combinationType>ONE</combinationType>
+         <id>063f4ced-b9c2-4d24-b42b-c741fd9aac06</id>
+         <iterationEntity>
+            <iterationType>ALL</iterationType>
+            <value></value>
+         </iterationEntity>
+         <testDataId>Data Files/LoginData</testDataId>
+      </testDataLink>
+      <variableLink>
+         <testDataLinkId>063f4ced-b9c2-4d24-b42b-c741fd9aac06</testDataLinkId>
+         <type>DATA_COLUMN</type>
+         <value>username</value>
+         <variableId>83df2915-50b5-4465-841f-cbcd829dbd94</variableId>
+      </variableLink>
+      <variableLink>
+         <testDataLinkId>063f4ced-b9c2-4d24-b42b-c741fd9aac06</testDataLinkId>
+         <type>DATA_COLUMN</type>
+         <value>password</value>
+         <variableId>fa43d71a-73c6-4d1a-8d1b-6244280087b9</variableId>
+      </variableLink>
+   </testCaseLink>
+</TestSuiteEntity>
---
+++ Test Suites/TestSuiteDemo1.ts
@@ -0,0 +1,38 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>TestSuiteDemo1</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient>zsyednajam@gmail.com;</mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>85e4feac-156e-43aa-bb50-93215f9e90bf</testSuiteGuid>
+   <testCaseLink>
+      <guid>864d7543-deb7-4ccd-ada7-9e0a9bcab98a</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Demo/ManualModeDemo</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>c04a6cfa-19b8-41ef-8704-859cf93e766e</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>false</isRun>
+      <testCaseId>Test Cases/Demo/RecordModeDemo</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>bcc7729d-dfac-45ae-8620-c5ca4766be48</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>false</isRun>
+      <testCaseId>Test Cases/Demo/ScriptModeDemo</testCaseId>
+      <variableLink>
+         <testDataLinkId></testDataLinkId>
+         <type>DEFAULT</type>
+         <value></value>
+         <variableId>deef4f24-10a9-4d1b-a7d4-ede7c504fc58</variableId>
+      </variableLink>
+   </testCaseLink>
+</TestSuiteEntity>
